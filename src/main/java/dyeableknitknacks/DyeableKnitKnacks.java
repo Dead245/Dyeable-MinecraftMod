0563cb01b7bb2ca874ad82227a7d16b8ff2f7ed6
Dead245 <116427900+Dead245@users.noreply.github.com>
2022-12-26 03:29:14 -0600

    added empty java file for mod and updated toml

---
+++ src/main/java/dyeableknitknacks/DyeableKnitKnacks.java
@@ -0,0 +1,7 @@
+package dyeableknitknacks;
+
+import net.minecraftforge.fml.common.Mod;
+@Mod(DyeableKnitKnacks.modid)
+public class DyeableKnitKnacks {
+    public static final String modid = "dyeableknitknacks";
+}
