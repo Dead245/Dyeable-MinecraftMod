4a573d1244e5d9e4c18852e6f981ed7417e9249a
Dead245 <116427900+Dead245@users.noreply.github.com>
2022-12-30 03:10:21 -0600

    Added functionality for if you click on an entity, it doesn't register the block clicked.

--- src/main/java/dyeableknitknacks/items/dyeGadget.java
+++ src/main/java/dyeableknitknacks/items/dyeGadget.java
@@ -5,16 +5,19 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.ClipContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.phys.BlockHitResult;
+import net.minecraftforge.client.event.RenderHighlightEvent;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
@@ -24,30 +27,42 @@ public class dyeGadget extends Item {
         super(properties);
     }
 
+    int entityID = 1;
 
-    //Open item GUI/change dye | Change dye of block?
+    void setEntityId(int id){ entityID = id;}
+
+    @Override
+    public InteractionResult useOn(UseOnContext p_41427_) {
+        return super.useOn(p_41427_);
+    }
+
+    //Open item GUI/change dye | Change dye of block |  Change dye of entity(sheep)
     @Override
     public InteractionResultHolder<ItemStack> use(Level lvl, Player plyr, InteractionHand intrHand) {
-        if(!lvl.isClientSide){
+        if(!lvl.isClientSide && entityID < 0){
             BlockHitResult ray = getPlayerPOVHitResult(lvl,plyr, ClipContext.Fluid.NONE);
             BlockPos lookPos = ray.getBlockPos();
             Block blkType = lvl.getBlockState(lookPos).getBlock();
-
             if(blkType == Blocks.AIR){
                 plyr.sendSystemMessage(Component.literal("Right clicked in the air."));
             } else{
-                plyr.sendSystemMessage(Component.literal("Right clicked on "+ blkType.getName().getString()));
+                plyr.sendSystemMessage(Component.literal("Right clicked on Block: "+ blkType.getName().getString()));
             }
+        } else if (!lvl.isClientSide && entityID >= 0) {
+            LivingEntity liveEnt = (LivingEntity) lvl.getEntity(entityID); //easier to get entity name through LivingEntity???
+            plyr.sendSystemMessage(Component.literal("Right clicked on Entity: " + liveEnt.getName().getString()));
         }
+        entityID = -1; //basically null?
         return super.use(lvl, plyr, intrHand);
     }
 
-    //Change dye of entity(sheep)
+    //Get ID of entity of right-clicked on one.
     @Override
     public InteractionResult interactLivingEntity(ItemStack itemStack, Player plyr, LivingEntity liveEntity, InteractionHand intrHand) {
-        plyr.sendSystemMessage(Component.literal("Right clicked a " + liveEntity.getName().getString()));
+        setEntityId(liveEntity.getId());
         return super.interactLivingEntity(itemStack, plyr, liveEntity, intrHand);
     }
+
 
     //Tooltip
     @Override
