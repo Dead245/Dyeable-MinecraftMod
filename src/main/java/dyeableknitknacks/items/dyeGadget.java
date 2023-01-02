bba4849af50d75844f69796218efeca8c8b4ea50
Dead245 <116427900+Dead245@users.noreply.github.com>
2023-01-02 05:40:50 -0600

    Expanded block functionality by allowing it to get the item name (ex. "red_carpet"). Added a check to see if the block was one that was dyed, and a list of the 16 colors. Also removed unnecessary setEntityID() function.

--- src/main/java/dyeableknitknacks/items/dyeGadget.java
+++ src/main/java/dyeableknitknacks/items/dyeGadget.java
@@ -5,7 +5,6 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.InteractionResultHolder;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
@@ -17,7 +16,6 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.phys.BlockHitResult;
-import net.minecraftforge.client.event.RenderHighlightEvent;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
@@ -27,16 +25,17 @@ public class dyeGadget extends Item {
         super(properties);
     }
 
-    int entityID = 1;
-
-    void setEntityId(int id){ entityID = id;}
-
+    int entityID = -1;
+    String[] colors = { "WHITE","ORANGE","MAGENTA","LIGHT_BLUE",
+                        "YELLOW","LIME","PINK","GRAY",
+                        "LIGHT_GREY","CYAN","PURPLE","BLUE",
+                        "BROWN","GREEN","RED","BLACK" };
     @Override
     public InteractionResult useOn(UseOnContext p_41427_) {
         return super.useOn(p_41427_);
     }
 
-    //Open item GUI/change dye | Change dye of block |  Change dye of entity(sheep)
+    //Open item GUI/change dye | Change dye of block |  Change dye of entity (sheep)
     @Override
     public InteractionResultHolder<ItemStack> use(Level lvl, Player plyr, InteractionHand intrHand) {
         if(!lvl.isClientSide && entityID < 0){
@@ -44,11 +43,25 @@ public class dyeGadget extends Item {
             BlockPos lookPos = ray.getBlockPos();
             Block blkType = lvl.getBlockState(lookPos).getBlock();
             if(blkType == Blocks.AIR){
+                //IS NOTHING
                 plyr.sendSystemMessage(Component.literal("Right clicked in the air."));
             } else{
-                plyr.sendSystemMessage(Component.literal("Right clicked on Block: "+ blkType.getName().getString()));
+                //IS BLOCK
+                String blockName = blkType.getDescriptionId().substring(blkType.getDescriptionId().lastIndexOf('.') + 1);
+                plyr.sendSystemMessage(Component.literal("Right clicked on Block: " + blockName ));
+
+                if(blockName.contains("_concrete") ||
+                   blockName.contains("_terracotta") ||
+                   blockName.contains("_wool") ||
+                   blockName.contains("_candle") ||
+                   blockName.contains("stained_glass") ||
+                   blockName.contains("_carpet")) {
+
+                    plyr.sendSystemMessage(Component.literal("IS DYEABLE BLOCK"));
+                }
             }
         } else if (!lvl.isClientSide && entityID >= 0) {
+            //IS ENTITY
             LivingEntity liveEnt = (LivingEntity) lvl.getEntity(entityID); //easier to get entity name through LivingEntity???
             plyr.sendSystemMessage(Component.literal("Right clicked on Entity: " + liveEnt.getName().getString()));
         }
@@ -59,7 +72,7 @@ public class dyeGadget extends Item {
     //Get ID of entity of right-clicked on one.
     @Override
     public InteractionResult interactLivingEntity(ItemStack itemStack, Player plyr, LivingEntity liveEntity, InteractionHand intrHand) {
-        setEntityId(liveEntity.getId());
+        entityID = liveEntity.getId();
         return super.interactLivingEntity(itemStack, plyr, liveEntity, intrHand);
     }
 
