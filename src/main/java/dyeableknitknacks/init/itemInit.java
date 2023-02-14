3fa093853ea494130e3a6e05ff6c29b49a69b179
Dead245 <116427900+Dead245@users.noreply.github.com>
2023-02-14 15:44:14 -0600

    Removed example_item

--- src/main/java/dyeableknitknacks/init/itemInit.java
+++ src/main/java/dyeableknitknacks/init/itemInit.java
@@ -11,6 +11,5 @@ import net.minecraftforge.registries.RegistryObject;
 public class itemInit {
     public static final DeferredRegister<Item> items = DeferredRegister.create(ForgeRegistries.ITEMS, DyeableKnitKnacks.modid);
 
-    public static final RegistryObject<Item> newItem = items.register("example_item", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_TOOLS)));
     public static final RegistryObject<dyeGadget> dyeGadget = items.register("dye_gadget", () -> new dyeGadget(new Item.Properties().tab(CreativeModeTab.TAB_TOOLS)));
 }
